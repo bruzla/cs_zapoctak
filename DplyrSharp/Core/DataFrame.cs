be51eea78df2b9a51ed964cce172996343c25679
Adam Bruzl <bruzl.00@gmail.com>
2025-05-28 18:47:13 +0200

    df adjustments

--- DplyrSharp/Core/DataFrame.cs
+++ DplyrSharp/Core/DataFrame.cs
@@ -1,125 +1,96 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 
 namespace DplyrSharp.Core;
 
-public sealed class DataFrame
+public partial class DataFrame
 {
     private readonly List<IDataColumn> _columns;
     private readonly Dictionary<string, int> _nameToIndex;
 
-    public IReadOnlyList<IDataColumn> Columns => _columns;
+    public int RowCount { get; init; }
+    public int ColCount { get; init; }
     public Schema Schema { get; }
 
     public DataFrame(IEnumerable<IDataColumn> columns)
     {
-        _columns = new List<IDataColumn>(columns);
+        _columns = columns.ToList(); // tady deep copy!!!!!!
+
         _nameToIndex = _columns
             .Select((c, i) => (c.Name, i))
             .ToDictionary(x => x.Name, x => x.i, StringComparer.Ordinal);
-        Schema = new Schema(_columns);
-    }
-    public DataFrame Materialise() => this;
 
-    public void WriteCsv(string path, IO.CsvOptions? options = null)
-        => IO.CsvWriter.WriteCsv(this, path, options);
+        Schema = new Schema(_columns);
+        ColCount = _columns.Count;
+        RowCount = ColCount == 0 ? 0 : _columns[0].RowCount;
+    }
+
+    public IReadOnlyList<IDataColumn> Columns => _columns;
 
     public IEnumerable<DataRow> Rows
     {
         get
         {
-            int rowCount = _columns.Count > 0 ? _columns[0].RowCount : 0;
-            for (int i = 0; i < rowCount; i++)
+            for (int i = 0; i < RowCount; i++)
                 yield return new DataRow(this, i);
         }
     }
 
     internal int GetColumnIndex(string name)
     {
-        if (!_nameToIndex.TryGetValue(name, out var idx))
+        if (!_nameToIndex.TryGetValue(name, out var index))
             throw new ArgumentException($"Column '{name}' not found.");
-        return idx;
-    }
-}
-
-public interface IDataColumn
-{
-    string Name { get; }
-    Type ClrType { get; }
-    int RowCount { get; }
-
-    BitArray? NullBitmap { get; }
-
-    object? GetValue(int rowIndex);
-}
-
-public sealed class DataColumn<T> : IDataColumn, IReadOnlyList<T>
-{
-    private readonly T[] _data;
-    private readonly BitArray _nullBitmap;
-
-    public string Name { get; }
-    public Type ClrType => typeof(T);
-    public int RowCount => _data.Length;
-    public BitArray NullBitmap => _nullBitmap;
-
-    public DataColumn(string name, T[] data, BitArray nullBitmap)
-    {
-        Name = name;
-        _data = data;
-        _nullBitmap = nullBitmap;
+        return index;
     }
 
-    public T this[int index] => _data[index];
-
-    public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)_data).GetEnumerator();
-    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => _data.GetEnumerator();
-    int IReadOnlyCollection<T>.Count => _data.Length;
-
-    public object? GetValue(int rowIndex)
+    private DataFrame SliceRows(IList<int> rows)
     {
-        if (_nullBitmap[rowIndex])
-            return null;
-        return _data[rowIndex]!;
+        var newCols = new List<IDataColumn>(Columns.Count);
+        foreach (var col in Columns)
+        {
+            var type = col.ClrType;
+            var data = Array.CreateInstance(type, rows.Count);
+            var nulls = new BitArray(rows.Count);
+            for (int i = 0; i < rows.Count; i++)
+            {
+                var v = col.GetValue(rows[i]);
+                if (v == null)
+                    nulls[i] = true;
+                data.SetValue(v, i);
+            }
+            var ctor = typeof(DataColumn<>)
+                .MakeGenericType(type)
+                .GetConstructor(new[] { typeof(string), data.GetType(), typeof(BitArray) });
+            var newCol = (IDataColumn)ctor!.Invoke(new object[] { col.Name, data, nulls });
+            newCols.Add(newCol);
+        }
+        return new DataFrame(newCols);
     }
-}
 
-public readonly struct DataRow
-{
-    private readonly DataFrame _df;
-    private readonly int _rowIndex;
-
-    internal DataRow(DataFrame df, int rowIndex)
+    public void WriteCsv(string path, IO.CsvOptions? options = null)
     {
-        _df = df;
-        _rowIndex = rowIndex;
+        IO.CsvWriter.WriteCsv(this, path, options);
     }
 
-    public object? this[string columnName] => _df.Columns[_df.GetColumnIndex(columnName)].GetValue(_rowIndex);
-
-    public object? this[int columnIndex] => _df.Columns[columnIndex].GetValue(_rowIndex);
-}
-
-public sealed class Schema
-{
-    public IReadOnlyList<ColumnInfo> Columns { get; }
-
-    public Schema(IEnumerable<IDataColumn> columns)
+    public override string ToString()
     {
-        Columns = columns.Select(c => new ColumnInfo(c.Name, c.ClrType)).ToList();
+        var sb = new StringBuilder();
+
+        // names
+        var colNames = _columns.Select(x => x.Name);
+        sb.AppendLine(string.Join(IO.CsvOptions.Default.Delimiter, colNames));
+
+        // values
+        foreach (var row in Rows)
+        {
+            sb.AppendLine(row.ToString());
+        }
+
+        return sb.ToString();
     }
-}
 
-public readonly struct ColumnInfo
-{
-    public string Name { get; }
-    public Type ClrType { get; }
-
-    public ColumnInfo(string name, Type clrType)
+    public void Print()
     {
-        Name = name;
-        ClrType = clrType;
+        Console.WriteLine(this.ToString());
     }
 }
